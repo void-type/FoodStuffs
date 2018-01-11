fdfec3e5eb96582952ae2045af3bdd2b763b6d93
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2018-01-11 00:15:08 -0700

    Removed browserlink

--- FoodStuffs.Web/Startup.cs
+++ FoodStuffs.Web/Startup.cs
@@ -1,4 +1,4 @@
-﻿using Core.Model.Services.DateTime;
+using Core.Model.Services.DateTime;
 using Core.Model.Services.Logging;
 using FoodStuffs.Data.Services;
 using FoodStuffs.Model.Interfaces.Services.Data;
@@ -12,52 +12,52 @@ using Microsoft.Extensions.Logging;
 
 namespace FoodStuffs.Web
 {
-    public class Startup
+  public class Startup
+  {
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
     {
-        public IConfiguration Configuration { get; }
-
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            app.UseStaticFiles();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    "default",
-                    "{controller=Home}/{action=Index}");
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Add framework services.
-            services.AddMvc();
-            services.AddSingleton(Configuration);
-
-            // Choose a db implementation
-            services.AddTransient<IFoodStuffsData, FoodStuffsEfSqlData>();
-            //services.AddTransient<IFoodStuffsData, FoodStuffsEfMemoryData>();
-
-            services.AddTransient<ILoggingService, ActionToAspNetLoggerAdapter>();
-            services.AddTransient<IDateTimeService, NowDateTimeService>();
-            services.AddTransient<HttpActionResultResponder>();
-        }
+      _configuration = configuration;
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+      }
+
+      app.UseStaticFiles();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+                  "default",
+                  "{controller=Home}/{action=Index}");
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      // Add framework services.
+      services.AddMvc();
+      services.AddSingleton(_configuration);
+
+      // Choose a db implementation
+      services.AddTransient<IFoodStuffsData, FoodStuffsEfSqlData>();
+      //services.AddTransient<IFoodStuffsData, FoodStuffsEfMemoryData>();
+
+      // Add other dependencies
+      services.AddTransient<ILoggingService, ActionToAspNetLoggerAdapter>();
+      services.AddTransient<IDateTimeService, NowDateTimeService>();
+      services.AddTransient<HttpActionResultResponder>();
+    }
+  }
 }
