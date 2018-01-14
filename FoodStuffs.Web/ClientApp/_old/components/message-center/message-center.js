e1696724517e75d1b8b2e2508b8d448af65b64dd
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2018-01-13 18:51:38 -0700

    Removed old vue app.

--- FoodStuffs.Web/ClientApp/_old/components/message-center/message-center.js
+++
@@ -1,6 +0,0 @@
-﻿require("./message-center.scss");
-
-Vue.component("message-center", {
-    template: require("./message-center.html"),
-    props: ["messages", "is-error"]
-});
--- FoodStuffs.Web/ClientApp/_old/store/createdRecipeModel.js
+++
@@ -1,9 +0,0 @@
-﻿// New recipe class structure
-module.exports = function () {
-    this.categories = [];
-    this.cookTimeMinutes = null;
-    this.directions = "";
-    this.ingredients = "";
-    this.name = "";
-    this.prepTimeMinutes = null;
-};
