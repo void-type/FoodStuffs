3b10ebcaf0eef3eb6c54ea89219398f9f0042e36
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2018-01-16 22:43:41 -0700

    Better Logging. Goes to C drive to support deployment.

--- FoodStuffs.Web/Program.cs
+++ FoodStuffs.Web/Program.cs
@@ -7,44 +7,48 @@ using System;
 
 namespace FoodStuffs.Web
 {
-  public class Program
-  {
-    public static IWebHost BuildWebHost(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((builderContext, config) =>
-            {
-              var env = builderContext.HostingEnvironment;
-              config.AddJsonFile($"appSecrets.{env.EnvironmentName}.json", true, true);
-            })
-            .UseStartup<Startup>()
-            .UseSerilog()
-            .Build();
-
-    public static int Main(string[] args)
+    public class Program
     {
-      Log.Logger = new LoggerConfiguration()
-          .MinimumLevel.Debug()
-          .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-          .MinimumLevel.Override("System", LogEventLevel.Warning)
-          .Enrich.FromLogContext()
-          .WriteTo.File("App_Data/Logs/log.txt", rollingInterval: RollingInterval.Day)
-          .CreateLogger();
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((builderContext, config) =>
+                {
+                    var env = builderContext.HostingEnvironment;
+                    config.AddJsonFile($"appSecrets.{env.EnvironmentName}.json", true, true);
+                })
+                .UseStartup<Startup>()
+                .UseSerilog()
+                .Build();
 
-      try
-      {
-        Log.Information("Starting web host");
-        BuildWebHost(args).Run();
-        return 0;
-      }
-      catch (Exception ex)
-      {
-        Log.Fatal(ex, "Host terminated unexpectedly");
-        return 1;
-      }
-      finally
-      {
-        Log.CloseAndFlush();
-      }
+        public static int Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.File($"C:/WebAppLogs/FoodStuffs-{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}_.log",
+                    rollingInterval: RollingInterval.Day,
+                    retainedFileCountLimit: 15,
+                    fileSizeLimitBytes: 10000000,
+                    rollOnFileSizeLimit: true)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                BuildWebHost(args).Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
     }
-  }
 }
