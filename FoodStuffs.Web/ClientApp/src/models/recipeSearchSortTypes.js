719049055d51e216ac9a8db2bc283cc2c826b044
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2018-03-11 17:56:19 -0600

    Changes to sorttypes model

--- FoodStuffs.Web/ClientApp/src/models/recipeSearchSortTypes.js
+++ FoodStuffs.Web/ClientApp/src/models/recipeSearchSortTypes.js
@@ -1,17 +1,14 @@
 ﻿export default [
   {
-    id: 0,
-    name: "chronological",
-    symbol: "&#x1F552;"
-  },
-  {
-    id: 1,
     name: "ascending",
     symbol: "&#9660;"
   },
   {
-    id: 2,
     name: "descending",
     symbol: "&#9650;"
+  },
+  {
+    name: "chronological",
+    symbol: "&#x1F552;"
   }
 ]
--- FoodStuffs.Web/ClientApp/src/models/recipesSearchParameters.js
+++ FoodStuffs.Web/ClientApp/src/models/recipesSearchParameters.js
@@ -1,8 +1,10 @@
-﻿export default class {
+﻿import recipeSearchSortTypes from "./recipeSearchSortTypes";
+
+export default class {
   constructor() {
     this.nameSearch = "";
     this.categorySearch = "";
-    this.sort = "ascending";
+    this.sort = recipeSearchSortTypes[0].name;
     this.page = 1;
     this.take = 30;
   }
