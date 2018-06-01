b84fd2c63cca8e850a01680855910efccc26214c
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2018-05-31 19:19:10 -0600

    Production and staging docker scripts

---
+++ Scripts/buildDocker-Production.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+cd ../
+
+docker build --build-arg env=Production -t foodstuffs-production .
+docker run -it --rm -p 3333:80 --name foodstuffs-production foodstuffs-production
--- Scripts/buildDocker-Staging.sh
+++ Scripts/buildDocker-Staging.sh
@@ -3,4 +3,4 @@
 cd ../
 
 docker build --build-arg env=Staging -t foodstuffs-staging .
-docker run -it --rm -p 3333:80 --name foodstuffs-prod foodstuffs-staging
+docker run -it --rm -p 3333:80 --name foodstuffs-staging foodstuffs-staging
