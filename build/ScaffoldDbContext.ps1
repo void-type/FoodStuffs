e1f62cb48280f32225da1c1ef5ce3e870b81865f
Jeff Schreiner <Schreinerj@prpa.org>
2019-02-13 10:53:10 -0700

    Fix scaffold script

--- build/ScaffoldDbContext.ps1
+++ build/ScaffoldDbContext.ps1
@@ -7,9 +7,9 @@ $contextName = "$($shortAppName)Context"
 $connectionString = Get-Content -Path "appsettings.Development.json" |
   ConvertFrom-Json |
   Select-Object -ExpandProperty ConnectionStrings |
-  Select-Object -ExpandProperty $projectName
+  Select-Object -ExpandProperty $shortAppName
 
-dotnet ef dbcontext scaffold "$connectionString" Microsoft.EntityFrameworkCore.SqlServer --force --context-dir "$contextDirectory" --context "$contextName" --output-dir "$dataModelsFolder"
+dotnet ef dbcontext scaffold "$connectionString" Microsoft.EntityFrameworkCore.SqlServer --force --context-dir "$contextDirectory" --context "$contextName" --output-dir "../$dataModelsFolder"
 
 Write-Host "Within $dataModelsFolder, update namespaces."
 Write-Host "Within $contextName, add using for models namespace."
