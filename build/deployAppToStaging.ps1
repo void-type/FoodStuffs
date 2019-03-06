f8ab55b85f44070da3c4915b4e1e9e9bb1921c1d
Jeff Schreiner <Jeff.S.Schreiner@gmail.com>
2019-03-05 22:19:36 -0700

    Check for artifacts before deploy

--- build/deployAppToStaging.ps1
+++ build/deployAppToStaging.ps1
@@ -1,5 +1,11 @@
 # Run this script as a server administrator from the scripts directory
+
 . ./util.ps1
+
+if (-not (Test-Path -Path "../artifacts")) {
+  Write-Error "No artifacts to deploy. Run build.ps1 before deploying."
+  exit 1
+}
 
 Push-Location -Path "../"
 New-Item -Path "$iisDirectoryStaging\app_offline.htm"
