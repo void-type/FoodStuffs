81339bd9337f7cf859f7ec95f55e02603378592d
Jeff Schreiner <Jeff.S.Schreiner@gmail.com>
2019-03-05 22:52:25 -0700

    Move file to API.

--- src/FoodStuffs.Web/ClientApp/src/webApi/index.js
+++ src/FoodStuffs.Web/ClientApp/src/webApi/index.js
@@ -23,4 +23,42 @@ export default {
       store.dispatch('app/setErrorMessage', 'Something went wrong. Try refreshing your browser or contact the administrator.');
     }
   },
+  saveDownloadedFile(response) {
+    const filename = response.headers['content-disposition']
+      .split('; ')
+      .filter(part => part.startsWith('filename'))[0]
+      .split('=')[1];
+
+    const contentType = response.headers['content-type'];
+    const blob = new Blob([response.data], { type: contentType });
+
+    try {
+      const linkElement = document.createElement('a');
+      linkElement.setAttribute('href', window.URL.createObjectURL(blob));
+      linkElement.setAttribute('download', filename);
+
+      const clickEvent = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: false,
+      });
+
+      linkElement.dispatchEvent(clickEvent);
+    } catch (e) {
+      window.navigator.msSaveBlob(blob, filename);
+    }
+  },
+  decodeFileDownloadFailureResponse(response) {
+    if (response.request.responseType !== 'arraybuffer') {
+      return response;
+    }
+
+    const decodedString = String.fromCharCode.apply(null, new Uint8Array(response.data));
+
+    if (decodedString.length <= 0) {
+      return '';
+    }
+
+    return Object.assign(response, { data: JSON.parse(decodedString) });
+  },
 };
