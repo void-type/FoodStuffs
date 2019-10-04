c12c57f948b47d53612eb705b46fa5c1b0467a45
Jeff Schreiner <Schreinerj@prpa.org>
2019-10-04 11:55:37 -0600

    ShouldProcess for deployments

--- build/deployAppToProduction.ps1
+++ build/deployAppToProduction.ps1
@@ -1,15 +1,8 @@
 # Run this script as a server administrator from the scripts directory
-
-param(
-  [switch] $Confirm
-)
+[CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = "High")]
+param()
 
 . ./util.ps1
-
-if (-not $Confirm) {
-  Write-Error "Please use -Confirm for pushing to production."
-  exit 1
-}
 
 if (-not (Test-Path -Path "../artifacts")) {
   Write-Error "No artifacts to deploy. Run build.ps1 before deploying."
@@ -17,7 +10,15 @@ if (-not (Test-Path -Path "../artifacts")) {
 }
 
 Push-Location -Path "../"
-New-Item -Path "$iisDirectoryProduction\app_offline.htm"
-ROBOCOPY "./artifacts" $iisDirectoryProduction /MIR
-Copy-Item -Path "$settingsDirectoryProduction\*" -Include "*.Production.json" -Recurse -Destination $iisDirectoryProduction
-Pop-Location
+
+try {
+  if ($PSCmdlet.ShouldProcess("$iisDirectoryProduction", "Deploy $shortAppName to Production.")) {
+    New-Item -Path "$iisDirectoryProduction\app_offline.htm"
+    Start-Sleep 5
+    ROBOCOPY "./artifacts" $iisDirectoryProduction /MIR /XF "$iisDirectoryProduction\app_offline.htm"
+    Copy-Item -Path "$settingsDirectoryProduction\*" -Include "*.Production.json" -Recurse -Destination $iisDirectoryProduction
+    Remove-Item -Path "$iisDirectoryProduction\app_offline.htm"
+  }
+} finally {
+  Pop-Location
+}
