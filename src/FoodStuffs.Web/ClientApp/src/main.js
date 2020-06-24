26559c6f17057e73128474fb8134ff604c3703c0
Jeff Schreiner <Schreinerj@prpa.org>
2020-06-24 10:55:00 -0600

    Plugin setup.

--- src/FoodStuffs.Web/ClientApp/src/main.js
+++ src/FoodStuffs.Web/ClientApp/src/main.js
@@ -1,11 +1,8 @@
 import '@babel/polyfill';
-import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
-Vue.use(BootstrapVue);
 
 new Vue({
   render: h => h(App),
