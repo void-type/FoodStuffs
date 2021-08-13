fe26a61344d2db2ce2fb5f0cf5db026926401286
Jeff Schreiner <Schreinerj@prpa.org>
2021-08-13 15:11:43 -0600

    Remove duplicate setting

--- build/util.ps1
+++ build/util.ps1
@@ -15,8 +15,6 @@ $testProjectFolder = "../tests/$projectName.Test"
 $webProjectFolder = "../src/$projectName.Web"
 $webClientProjectFolder = "$webProjectFolder/ClientApp"
 
-$testProjectFolder = "../tests/$projectName.Test"
-
 $iisDirectoryProduction = "\\server2\wwwroot\$($projectName)"
 $settingsDirectoryProduction = "\\server2\Servers\webAppSettings\$($projectName)"
 
