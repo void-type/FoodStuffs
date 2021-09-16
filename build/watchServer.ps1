4a9cd474c1be9b11cf0afa4c6939cdd2e1423064
Jeff Schreiner <Schreinerj@prpa.org>
2021-09-16 11:44:11 -0600

    Use single quote

--- build/watchServer.ps1
+++ build/watchServer.ps1
@@ -2,7 +2,7 @@ Push-Location -Path "$PSScriptRoot/../"
 . ./build/util.ps1
 
 try {
-  dotnet watch run --project "$webProjectFolder" --configuration "Debug" --launch-profile 'Kestrel (Development)'
+  dotnet watch run --project "$webProjectFolder" --configuration 'Debug' --launch-profile 'Kestrel (Development)'
 
 } finally {
   Pop-Location
