e249b430f3a38624f51cef449469278249581027
Jeff Schreiner <Jeff.S.Schreiner@gmail.com>
2022-03-20 01:16:13 -0600

    Update deployment vars

--- build/util.ps1
+++ build/util.ps1
@@ -5,8 +5,8 @@ $webProjectFolder = "./src/$projectName.Web"
 $webClientProjectFolder = "$webProjectFolder/ClientApp"
 $testProjectFolder = "./tests/$projectName.Test"
 
-$iisDirectoryProduction = "\\server2\wwwroot\$($projectName)"
-$settingsDirectoryProduction = "\\server2\Servers\webAppSettings\$($projectName)"
+$iisDirectoryProduction = "\\server2\DeployedApps\apps\$($projectName)"
+$settingsDirectoryProduction = "\\server2\Servers\AppConfigs\$($projectName)"
 
-$iisDirectoryStaging = "\\server2\wwwroot\$($projectName)Test"
+$iisDirectoryStaging = "\\server2\DeployedApps\apps\$($projectName)Test"
 $settingsDirectoryStaging = "$settingsDirectoryProduction"
