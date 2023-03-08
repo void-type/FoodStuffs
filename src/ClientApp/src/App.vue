a84564f36eb764a39b387e30548631eed0eafbad
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2023-03-07 21:06:17 -0700

    Fix preference switches in darkmode

--- src/ClientApp/src/App.vue
+++ src/ClientApp/src/App.vue
@@ -170,7 +170,7 @@ body.bg-dark {
     --bs-accordion-btn-active-icon: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23ffffff'%3e%3cpath fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/%3e%3c/svg%3e");
   }
 
-  .form-check-input {
+  .form-check:not(.form-switch) .form-check-input {
     background-color: var(--bs-dark);
     border-color: var(--bs-light);
 
