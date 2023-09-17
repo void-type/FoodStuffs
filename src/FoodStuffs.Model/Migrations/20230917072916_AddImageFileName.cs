f58bfe07e26fe3712c094c13010b08118ce98e72
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2023-09-17 15:15:18 -0600

    Format

--- src/FoodStuffs.Model/Migrations/20230917072916_AddImageFileName.cs
+++ src/FoodStuffs.Model/Migrations/20230917072916_AddImageFileName.cs
@@ -2,49 +2,48 @@
 
 #nullable disable
 
-namespace FoodStuffs.Model.Migrations
+namespace FoodStuffs.Model.Migrations;
+
+/// <inheritdoc />
+public partial class AddImageFileName : Migration
 {
     /// <inheritdoc />
-    public partial class AddImageFileName : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "FileExtension",
-                table: "Blob");
+        migrationBuilder.DropColumn(
+            name: "FileExtension",
+            table: "Blob");
 
-            migrationBuilder.AddColumn<string>(
-                name: "FileName",
-                table: "Image",
-                type: "nvarchar(450)",
-                nullable: false,
-                defaultValueSql: "NEWID()");
+        migrationBuilder.AddColumn<string>(
+            name: "FileName",
+            table: "Image",
+            type: "nvarchar(450)",
+            nullable: false,
+            defaultValueSql: "NEWID()");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Image_FileName",
-                table: "Image",
-                column: "FileName",
-                unique: true);
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_Image_FileName",
+            table: "Image",
+            column: "FileName",
+            unique: true);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropIndex(
-                name: "IX_Image_FileName",
-                table: "Image");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropIndex(
+            name: "IX_Image_FileName",
+            table: "Image");
 
-            migrationBuilder.DropColumn(
-                name: "FileName",
-                table: "Image");
+        migrationBuilder.DropColumn(
+            name: "FileName",
+            table: "Image");
 
-            migrationBuilder.AddColumn<string>(
-                name: "FileExtension",
-                table: "Blob",
-                type: "nvarchar(max)",
-                nullable: false,
-                defaultValue: "");
-        }
+        migrationBuilder.AddColumn<string>(
+            name: "FileExtension",
+            table: "Blob",
+            type: "nvarchar(max)",
+            nullable: false,
+            defaultValue: "");
     }
 }
