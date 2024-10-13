bea9e54e083af96f314b15068334daf76da06e6e
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2024-10-13 00:07:23 -0600

    Message center enhancements

--- src/ClientApp/src/components/AppMessageCenter.vue
+++ src/ClientApp/src/components/AppMessageCenter.vue
@@ -59,14 +59,14 @@ const { clearMessage, clearMessageTimeout } = messageStore;
     left: 0;
     right: 0;
     // padding: 0.8rem;
-    background-color: rgba(var(--bs-dark-rgb), 0.9);
+    background-color: rgba(var(--bs-body-bg-rgb), 0.8);
     border-radius: var(--bs-border-radius);
 
     div.alert {
       margin: 0;
       // text-align: center;
       margin-top: 0.2em;
-      --bs-alert-padding-x: 0.5rem;
+      --bs-alert-padding-x: 1rem;
       --bs-alert-padding-y: 0.5rem;
     }
 
