7bdba2aa1f60ea5cf23b9ceba64c90ecf5f036e8
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2025-08-23 22:39:14 -0600

    Rebuild index if out of date

--- src/FoodStuffs.Web/Startup/EnsureIndexHostedService.cs
+++ src/FoodStuffs.Web/Startup/EnsureIndexHostedService.cs
@@ -1,11 +1,12 @@
-﻿
+﻿using FoodStuffs.Model.Events.Recipes;
 using FoodStuffs.Model.Search;
 using FoodStuffs.Model.Search.Recipes;
+using FoodStuffs.Model.Search.Recipes.Models;
 
 namespace FoodStuffs.Web.Startup;
 
 /// <summary>
-/// This service will rebuild the index on startup if it doesn't exist.
+/// This service will rebuild the index on startup if it doesn't exist or if it's corrupted.
 /// </summary>
 public class EnsureIndexHostedService : IHostedService
 {
@@ -22,12 +23,45 @@ public class EnsureIndexHostedService : IHostedService
     {
         using var scope = _serviceProvider.CreateScope();
 
-        var index = scope.ServiceProvider.GetRequiredService<IRecipeIndexService>();
+        var needsRebuild = false;
+
+        // Check if index directories exist
         var config = scope.ServiceProvider.GetRequiredService<SearchSettings>();
 
         if (!Directory.Exists(config.GetIndexFolder(RecipeSearchConstants.INDEX_NAME)) || !Directory.Exists(config.GetTaxonomyFolder(RecipeSearchConstants.INDEX_NAME)))
         {
             _logger.LogWarning("Recipe index is missing on startup. Rebuilding.");
+            needsRebuild = true;
+        }
+        else
+        {
+            // Test the index by performing a simple search
+            try
+            {
+                var testRequest = new SearchRecipesRequest(
+                    SearchText: null,
+                    CategoryIds: null,
+                    AllCategories: false,
+                    IsForMealPlanning: null,
+                    SortBy: null,
+                    RandomSortSeed: null,
+                    IsPagingEnabled: true,
+                    Page: 1,
+                    Take: 5);
+
+                var searchHandler = scope.ServiceProvider.GetRequiredService<SearchRecipesHandler>();
+                await searchHandler.Handle(testRequest, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Recipe index test failed. Index may be corrupted. Rebuilding.");
+                needsRebuild = true;
+            }
+        }
+
+        if (needsRebuild)
+        {
+            var index = scope.ServiceProvider.GetRequiredService<IRecipeIndexService>();
             await index.RebuildAsync(cancellationToken);
         }
     }
