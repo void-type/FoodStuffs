f285487de2617718373ef412090f2c0c62995032
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2025-11-21 22:08:06 -0700

    Cleaner randomize button.

--- src/ClientApp/src/pages/HomePage.vue
+++ src/ClientApp/src/pages/HomePage.vue
@@ -60,7 +60,7 @@ onUnmounted(() => {
   <div class="container-xxl">
     <div class="btn-toolbar mt-2">
       <button
-        class="btn btn-outline-secondary py-0 px-2 ms-auto"
+        class="btn p-0 fs-5 ms-auto"
         aria-label="Randomize"
         title="Randomize"
         @click="randomize"
